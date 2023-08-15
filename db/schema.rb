c167fb60384bbea0909080120963be04c30a93b3
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-15 12:36:06 +0500

    Generate a migration to handle many to many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_065845) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_072143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_065845) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_sections_on_user_id"
+  end
+
+  create_table "sections_transactions", force: :cascade do |t|
+    t.bigint "section_id", null: false
+    t.bigint "transaction_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_sections_transactions_on_section_id"
+    t.index ["transaction_id"], name: "index_sections_transactions_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -39,5 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_065845) do
   end
 
   add_foreign_key "sections", "users"
+  add_foreign_key "sections_transactions", "sections"
+  add_foreign_key "sections_transactions", "transactions"
   add_foreign_key "transactions", "sections"
 end
