6b0505c362216ad2c7bcd5d046483ff45c602fb1
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-15 16:38:28 +0500

    Update devise configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      users_path
+    else
+      redirect_to new_user_session_path unless devise_controller?
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'splash/index', to: "splash#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'splash#index'
   # Defines the root path route ("/")
   # root "articles#index"
+
+
 end
