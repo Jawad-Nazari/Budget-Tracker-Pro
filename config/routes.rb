ebe486444211986405f88371bb7cee13c6739f30
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-15 17:11:50 +0500

    Generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+ 
   devise_for :users
   # get 'splash/index', to: "splash#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
