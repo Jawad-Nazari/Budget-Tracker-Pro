8f81ce12ea94aaad35cc4c3d3443f73667e8edb6
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-15 12:58:43 +0500

    Updated the relationships with refrence keys

--- db/migrate/20230815062401_create_transactions.rb
+++ db/migrate/20230815062401_create_transactions.rb
@@ -3,7 +3,6 @@ class CreateTransactions < ActiveRecord::Migration[7.0]
     create_table :transactions do |t|
       t.string :name
       t.integer :amount
-      t.references :section, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_072143) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_075230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,10 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_072143) do
   create_table "transactions", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
-    t.bigint "section_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["section_id"], name: "index_transactions_on_section_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,5 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_072143) do
   add_foreign_key "sections", "users"
   add_foreign_key "sections_transactions", "sections"
   add_foreign_key "sections_transactions", "transactions"
-  add_foreign_key "transactions", "sections"
+  add_foreign_key "transactions", "users"
 end
