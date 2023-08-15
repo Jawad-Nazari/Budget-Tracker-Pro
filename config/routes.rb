0cef083b5c306c1bde777890f975fcbb95367ca1
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-15 15:01:30 +0500

    Install devise and initial configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'splash/index'
+  # get 'splash/index', to: "splash#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'splash#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
