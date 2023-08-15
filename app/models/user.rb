09c41d32d883657ea0f2e6a44de91a54f402aab1
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-15 13:26:02 +0500

    Add associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :sections, foreign_key: :user_id, dependent: :delete_all
+  has_many :Transactions, foreign_key: :user_id, dependent: :delete_all
+
+  validates :name, presence: true, length: { maximum: 60 }
 end
