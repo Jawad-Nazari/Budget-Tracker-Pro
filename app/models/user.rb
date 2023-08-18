e4a75f43550efaac5ecf7d7ac34f6573fb887cb5
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-08-18 05:42:15 +0500

    Update the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :sections, foreign_key: :user_id, dependent: :delete_all
-  has_many :Transactions, foreign_key: :user_id, dependent: :delete_all
+  has_many :sections, foreign_key: :user_id, dependent: :destroy
+  has_many :payments, foreign_key: :user_id, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 60 }
 end
